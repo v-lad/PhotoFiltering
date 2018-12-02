7f6a91beebf19db4f6e5e57f3620ba2598b29d59
Vlad Novosadenko <novosadenko.v23@gmail.com>
2018-12-02 21:28:06 +0200

    First try to deploy on Heroku

--- PhotoFilter/settings.py
+++ PhotoFilter/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,3 +134,7 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "live-static-files", "media-root")
+
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn PhotoFilter.wsgi --log-file -
